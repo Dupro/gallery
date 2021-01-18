2165f3263e12197e23a27646990af2511db37bea
Dupro <duskoprokopijevic@gmail.com>
2021-01-18 20:39:54 +0100

    edit specific photo
    CKeditor 5
    edit photos

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,9 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- WYSIWYG -->
+    <script src="https://cdn.ckeditor.com/ckeditor5/24.0.0/classic/ckeditor.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
